dfeee1aa0a4b306d5ffe1b0d7b17edb7c8fb8282
Markus Skyttner <markussk@kth.se>
2020-11-24 13:35:30 +0100

    update pkgdown docs

--- devel/dev.R
+++ devel/dev.R
@@ -53,6 +53,9 @@ pkgdown::build_site()
 
 use_build_ignore(files = "devel")
 
+# test building of vignettes without any downloaded data present
 unlink(institutions_cfg()$zip)
 unlink(institutions_cfg()$db)
 build_vignettes()
+
+use_github_action("check-standard")
