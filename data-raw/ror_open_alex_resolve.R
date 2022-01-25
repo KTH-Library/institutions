37350f71e65b0f0e81873439e18e975e31e60ecf
Markus Skyttner <markussk@kth.se>
2022-01-25 10:53:38 +0100

    add example script to resolve missing ror_id using Open Alex API from organization name and country

---
+++ data-raw/ror_open_alex_resolve.R
@@ -0,0 +1,77 @@
+library(purrr)
+library(readr)
+library(progress)
+
+# filter based on data in a CSV file
+# (if using Google Drive as source, in the spreadsheet, ...
+# select show blank ror_ids and copy to clipboard ...
+# and save in data-raw/missing_ror_id.tsv
+mr <-
+  readr::read_tsv("data-raw/missing_ror_id.tsv")
+
+# optional filters
+data <-
+  mr # %>% filter(Comment == "Can't find a ROR")
+
+# fcn to iterate over those names
+openalex_guess <- function(data) {
+
+  orgz <- data$Name_eng
+  countriez <- data$Country_name
+  lookups <- paste0(orgz, ", ", countriez)
+
+  pb <- progress_bar$new(
+    format = "  open alex resolving [:bar] :percent eta: :eta",
+    total = length(lookups), clear = FALSE, width = 60)
+
+  candidates <- purrr::possibly(
+    .f = function(x) {
+      pb$tick()
+      Sys.sleep(1 / 100)
+      geocode_openalex(x) %>%
+      bind_cols(org_lookup = x) %>%
+      select(org_lookup, everything())
+    },
+    otherwise = data.frame()
+  )
+
+  openalex_guesses <-
+    lookups %>% map_df(candidates)
+
+  openalex_guesses %>%
+    left_join(
+      tibble(org_lookup = lookups, country_name = countriez),
+        by = c("org_lookup")) %>%
+    filter(country_country_name == country_name)
+
+}
+
+# do the resolution of names
+# takes around 5 min for approx 1800 orgnames
+# NB: no throttling other than 10 ms between requests
+guesses <-
+  openalex_guess(data)
+
+# add jaro-winkler score for "good matches" based on names
+score <-
+  data %>%
+  mutate(key = paste0(Name_eng, ", ", Country_name)) %>%
+  right_join(guesses %>% select(key = org_lookup, everything()), by = "key") %>%
+  mutate(jw_score = stringdist::stringdist(Name_eng, name, "jw"))
+
+# arrange the list with the best scoring records at the top
+out <-
+  score %>%
+  arrange(jw_score) %>%
+  filter(jw_score < 0.2) %>%
+  select(
+    score, Name_eng, name,
+    Unified_org_id, Country_name, ror_id = id, addresses_lat, addresses_lng,
+    everything()
+  )
+
+# inspect
+out %>%  View()
+
+# export
+readr::write_csv(out, "data-raw/ror_gap_fill.csv")
